1dcd903f11894c69099de3901086f406b6790bce
AndreyKazakov <33053969+AndreyKazakov@users.noreply.github.com>
2018-08-13 23:23:55 +0300

    Homework for lessons 1 and 2. Added classes Calculator and Main in a package lessons_1_2

---
+++ src/main/java/lessons_1_2/Calculator.java
@@ -0,0 +1,125 @@
+package lessons_1_2;
+
+import java.util.Scanner;
+
+/**
+ * Performs arithmetic operations with two fractional numbers.
+ *
+ * @author Andrey Kazakov by Aplana
+ * @see #readDataFromConsole() method - for reading data from console
+ * @see #add(double, double) method - for addition operation
+ * @see #sub(double, double) method - for subtraction operation
+ * @see #mul(double, double) method - for multiplication operation
+ * @see #div(double, double) method - for division operation
+ * @see #performCalculation() method - for the whole calculation process
+ */
+
+public class Calculator {
+
+    public double operand1;
+    public double operand2;
+    public String operation;
+
+    /**
+     * Reads data from console.
+     * It reads both of operands as well as the sign of operation.
+     */
+    public void readDataFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        operand1 = Double.valueOf(scanner.next().replace(",", "."));
+        // we read String value, then make replacement of a comma to a period for user convenience,  then convert String value to double.
+
+        System.out.println("Enter the second number:");
+        operand2 = Double.valueOf(scanner.next().replace(",", ".")); //same as line 32
+
+        System.out.println("Choose the sign of operation (available values: + - * /):");
+        operation = scanner.next().trim(); // trim helps us to avoid useless spaces
+
+        scanner.close();
+    }
+
+    /**
+     * Performs addition of two double values.
+     * @param value1 - 1st operand
+     * @param value2 - 2nd operand
+     * @return double value as a result of addition
+     */
+    public double add(double value1, double value2) {
+        double result = value1 + value2;
+        return result;
+    }
+
+    /**
+     * Performs subtraction between two double values.
+     * @param value1 - 1st operand
+     * @param value2 - 2nd operand
+     * @return double value as a result of subtraction
+     */
+    public double sub(double value1, double value2) {
+        double result = value1 - value2;
+        return result;
+    }
+
+    /**
+     * Performs multiplication of two double values.
+     * @param value1 - 1st operand
+     * @param value2 - 2nd operand
+     * @return double value as a result of multiplication
+     */
+    public double mul(double value1, double value2) {
+        double result = value1 * value2;
+        return result;
+    }
+
+    /**
+     * Divide 1st operand by 2nd operand. This method throws ArithmeticException in case when the 2nd operand is zero.
+     * @param value1 - double value; it's dividend
+     * @param value2 - double value: it's divisor
+     * @return double value as a result of division
+     */
+    public double div(double value1, double value2) {
+        if (value2 != 0) {
+            double result = value1 / value2;
+            return result;
+        } else {
+            throw new ArithmeticException("Division by zero");
+        }
+    }
+
+    /**
+     * Performs the whole process of calculation
+     */
+    public void performCalculation() {
+        double result;
+
+        readDataFromConsole(); //reads data from console
+
+        switch (operation) { //selects method for calculation according to entered sign
+            case "+":
+                result = add(operand1, operand2);
+                break;
+            case "-":
+                result = sub(operand1, operand2);
+                break;
+            case "*":
+                result = mul(operand1, operand2);
+                break;
+            case "/":
+                try {
+                    result = div(operand1, operand2);
+                } catch (ArithmeticException ae) {      //in case of exception we show a message for user and stop program
+                    System.out.println("Division by zero is not allowed");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Not allowed operation has been entered");  //if user enters incorrect operation we show message and stop programm
+                return;
+        }
+
+        System.out.printf("Result is: %.4f", result); //shows calculated result formatted
+    }
+
+}
--- src/main/java/lessons_1_2/Main.java
+++ src/main/java/lessons_1_2/Main.java
@@ -1,7 +1,10 @@
 package lessons_1_2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Calculator calculator = new Calculator(); // creates object of a calculator
+        calculator.performCalculation();
     }
 }
